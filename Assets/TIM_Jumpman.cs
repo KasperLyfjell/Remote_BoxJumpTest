95e19958d70af253538a839275e216f9d69d28a5
Kasper Lyfjell <kasper.lyfjell@hotmail.no>
2021-01-22 11:55:54 +0100

    Initialize Project Files

---
+++ Assets/TIM_Jumpman.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TIM_Jumpman : MonoBehaviour
+{
+    private void Update() {
+         if (Input.GetKeyDown(KeyCode.Space)) {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 200);
+        }
+    }
+}
